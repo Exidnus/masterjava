d5e7c5a85a3fd6a9374032d94e711bc542c10d0d
Grigory Kislin <javawebinar@yandex.ru>
2016-09-07 01:48:31 +0300

    Prepare to Matrix implementation

--- src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
+++ src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
@@ -1,23 +1,49 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * gkislin
  * 03.07.2016
  */
 public class MainMatrix {
-    // Multiplex matrix
     private static final int MATRIX_SIZE = 1000;
     private static final int THREAD_NUMBER = 10;
 
-    public static void main(String[] args) {
-        final int[][] matrixA = new int[MATRIX_SIZE][MATRIX_SIZE];
-        final int[][] matrixB = new int[MATRIX_SIZE][MATRIX_SIZE];
+    private final static ExecutorService executor = Executors.newFixedThreadPool(MainMatrix.THREAD_NUMBER);
 
-        long start = System.currentTimeMillis();
-        final int[][] matrixC =  MatrixUtil.singleThreadMultiply(matrixA, matrixB);
-        System.out.println("Single thread multiplication time, sec: " + (System.currentTimeMillis() - start)/1000.);
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final int[][] matrixA = MatrixUtil.create(MATRIX_SIZE);
+        final int[][] matrixB = MatrixUtil.create(MATRIX_SIZE);
 
-        // TODO implement parallel multiplication matrixA*matrixB
-        // TODO compare wih matrixC;
+        double singleThreadSum = 0.;
+        double concurrentThreadSum = 0.;
+        for (int i = 0; i < 5; i++) {
+            long start = System.currentTimeMillis();
+            final int[][] matrixC = MatrixUtil.singleThreadMultiply(matrixA, matrixB);
+            double duration = (System.currentTimeMillis() - start) / 1000.;
+            out("Single thread time, sec: %.3f", duration);
+            singleThreadSum += duration;
+
+            start = System.currentTimeMillis();
+            final int[][] concurrentMatrixC = MatrixUtil.concurrentMultiply(matrixA, matrixB, executor);
+            duration = (System.currentTimeMillis() - start) / 1000.;
+            out("Concurrent thread time, sec: %.3f", duration);
+            concurrentThreadSum += duration;
+
+            if (!MatrixUtil.compare(matrixC, concurrentMatrixC)) {
+                System.err.println("Comparison failed");
+                break;
+            }
+        }
+        executor.shutdown();
+        out("\nAverage single thread time, sec: %.3f", singleThreadSum / 5.);
+        out("Average concurrent thread time, sec: %.3f", concurrentThreadSum / 5.);
+    }
+
+    private static void out(String format, double ms) {
+        System.out.println(String.format(format, ms));
     }
 }
--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,10 +1,19 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+
 /**
  * gkislin
  * 03.07.2016
  */
 public class MatrixUtil {
+
+    // TODO implement parallel multiplication matrixA*matrixB
+    public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
+        return null;
+    }
 
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
@@ -20,5 +29,29 @@ public class MatrixUtil {
             }
         }
         return matrixC;
+    }
+
+    public static int[][] create(int size) {
+        int[][] matrix = new int[size][size];
+        Random rn = new Random();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = rn.nextInt(10);
+            }
+        }
+        return matrix;
+    }
+
+    public static boolean compare(int[][] matrixA, int[][] matrixB) {
+        final int matrixSize = matrixA.length;
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                if (matrixA[i][j] != matrixB[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
