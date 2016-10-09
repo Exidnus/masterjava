f6a840cae4cf07bfe12e27aefd65739dda00f595
Dmitriy_Varygin <dmitriy.varygin@yandex.ru>
2016-10-09 13:52:44 +0500

    implemented multithreading variant uploading xml

--- export/src/main/java/ru/javaops/masterjava/web/UploadServlet.java
+++ export/src/main/java/ru/javaops/masterjava/web/UploadServlet.java
@@ -1,10 +1,8 @@
 package ru.javaops.masterjava.web;
 
 import com.google.common.base.Strings;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileItemIterator;
-import org.apache.commons.fileupload.FileItemStream;
-import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.*;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -17,6 +15,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * @author Varygin DV {@literal <OUT-Varygin-DV@mail.ca.sbrf.ru>}
@@ -25,6 +26,7 @@ import java.io.InputStream;
 public class UploadServlet extends HttpServlet {
 
     private final UserServiceXml userServiceXml = UserServiceXml.getUserServiceXml();
+    private final ExecutorService service = Executors.newFixedThreadPool(4);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -35,17 +37,31 @@ public class UploadServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        final ServletFileUpload upload = new ServletFileUpload();
+        final DiskFileItemFactory factory = new DiskFileItemFactory();
+        final ServletFileUpload upload = new ServletFileUpload(factory);
         try {
-            final FileItemIterator itemIterator = upload.getItemIterator(req);
-            while (itemIterator.hasNext()) {
-                final FileItemStream stream = itemIterator.next();
-                if (!Strings.isNullOrEmpty(stream.getName())) {
-                    try (InputStream is = stream.openStream()) {
-                        userServiceXml.saveUsersFromXmlToBD(is);
-                    }
+            final List<FileItem> fileItems = upload.parseRequest(req);
+            final List<Future> futures = new ArrayList<>();
+            fileItems.stream()
+                    .filter(file -> !Strings.isNullOrEmpty(file.getName()))
+                    .forEach(file -> {
+                        final Future<?> future = service.submit(() -> {
+                            try (InputStream is = file.getInputStream()) {
+                                userServiceXml.saveUsersFromXmlToBD(is);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+                        futures.add(future);
+                    });
+
+            futures.forEach(future -> {
+                try {
+                    future.get();
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
                 }
-            }
+            });
         } catch (FileUploadException e) {
             throw new RuntimeException(e);
         }
