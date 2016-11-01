b29e3e941064e5aa130d2876134584abbd4ce5f0
Grigory Kislin <javawebinar@yandex.ru>
2016-11-01 23:11:59 +0300

    5_4_add_config

---
+++ common/src/main/java/ru/javaops/masterjava/config/Configs.java
@@ -0,0 +1,15 @@
+package ru.javaops.masterjava.config;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+/**
+ * gkislin
+ * 01.11.2016
+ */
+public class Configs {
+
+    public static Config getConfig(String resource) {
+        return ConfigFactory.parseResources(resource).resolve();
+    }
+}
