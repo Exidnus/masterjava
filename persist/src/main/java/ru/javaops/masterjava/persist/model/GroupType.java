87078e03361de55a0c3a7a3a1eba5cfcaf3c73fe
Dmitriy_Varygin <dvarygin@at-consulting.ru>
2016-11-03 14:21:00 +0300

    Added cities, groups, projects to initDB and GroupType

---
+++ persist/src/main/java/ru/javaops/masterjava/persist/model/GroupType.java
@@ -0,0 +1,9 @@
+package ru.javaops.masterjava.persist.model;
+
+/**
+ * @author Varygin DV {@literal <OUT-Varygin-DV@mail.ca.sbrf.ru>}
+ */
+public enum GroupType {
+    CURRENT,
+    FINISHED
+}
--- persist/src/main/java/ru/javaops/masterjava/persist/model/UserFlag.java
+++ persist/src/main/java/ru/javaops/masterjava/persist/model/UserFlag.java
@@ -7,5 +7,5 @@ package ru.javaops.masterjava.persist.model;
 public enum UserFlag {
     active,
     deleted,
-    superuser;
+    superuser
 }
